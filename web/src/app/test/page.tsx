7d21d558b507bbec9e8b2242d251c0bf182a8712
leoWeitz <lweitz@itba.edu.ar>
2025-03-29 14:10:50 -0300

    add localstorage utils

--- web/src/app/test/page.tsx
+++ web/src/app/test/page.tsx
@@ -1,7 +1,47 @@
+'use client'
+
 import { NameCard } from "@/components/ui/nameCard";
+import { getRandomFlashcard, initializeTest, removeFlashcard } from "@/utils/localstorageUtils";
+import { get } from "http";
+import { useEffect, useState } from "react";
 
 export default function TestPage() {
+    const data = [
+        { concept: "Opcion 1", definition: "A placeholder definition for John Doe."},
+        { concept: "Opcion 2", definition: "A placeholder definition for Jane Smith."},
+        { concept: "Opcion 3", definition: "A placeholder definition for Alice Johnson."},
+    ];
+    const [random, setRandom] = useState();
+    const [cardjson, setCardjson] = useState<{ concept: string; definition: string; index: Number} | null>(null);
+
+    useEffect(() => {
+        
+        const randomFlashCard = async () => {
+            const flashcard = await getRandomFlashcard();
+            setCardjson({
+                concept: flashcard?.card?.concept || "",
+                definition: flashcard?.card?.definition || "",
+                index: flashcard?.index || 0,
+            });
+        };
+        randomFlashCard();
+    }, []);
+    
     return <div>
-        <NameCard concept="Pipshas" />
+        
+        <NameCard concept={cardjson?.concept||""} description={cardjson?.definition} />
+        <button
+            onClick={ () => {
+                if (cardjson) {
+                    // Remove the flashcard from local storage
+                    const updatedData = removeFlashcard(cardjson.index?.valueOf()||0);
+
+                    // Redirect to "test2" page
+                    //window.location.reload();
+                }
+            }}
+        >
+            Delete Flashcard
+        </button>
     </div>;
 }
